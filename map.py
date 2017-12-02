0312c190b50deb6d97e524e1f01024f41062d718
kanav-raina <kanavraina98@gmail.com>
2017-12-03 03:06:20 +0530

    map application of python

---
+++ map.py
@@ -0,0 +1,23 @@
+import folium
+import pandas
+
+data=pandas.read_csv("volcanoes.txt")
+lat=list(data['LAT'])
+lon=list(data['LON'])
+elev=list(data['ELEV'])
+
+def color_producer(elevation):
+    if elevation <1000:
+        return "green"
+    else:
+        return "red"
+
+map=folium.Map(location=[38.58,-99.09])
+fg=folium.FeatureGroup(name="My Map")
+
+for lt,ln,el in zip(lat,lon,elev):
+    fg.add_child(folium.CircleMarker(location=[lt,ln],radius=6,popup=str(el)+"m",fill_color=color_producer(el),color='grey',fill_opacity=0.7))
+
+
+map.add_child(fg)
+map.save("Map1.html")
