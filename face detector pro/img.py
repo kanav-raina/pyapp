e2d6587fb3524233d9096651e3776895541d39e7
kanav-raina <kanavraina98@gmail.com>
2017-12-06 23:02:30 +0530

    face detector program using opencv library

---
+++ face detector pro/img.py
@@ -0,0 +1,13 @@
+import cv2
+
+img=cv2.imread("galaxy.jpg",1)
+
+print(type(img))
+print(img)
+print(img.shape)
+print(img.ndim)
+
+resized_img=cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/4)))
+cv2.imshow("Galaxy",resized_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
